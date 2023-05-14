b309e96ad8e3904e1ec255092ac1f2b76309be44
Andy.R <avroberts@outlook.com>
2023-05-14 17:19:01 +0100

    create postcode data set

--- src/Crimson.Model/Price.cs
+++ src/Crimson.Model/Price.cs
@@ -3,23 +3,38 @@ namespace Crimson.Model
 {
     public class PriceRecord
     {
-        public string? Outcode { get; set; }
-        public string? Postcode { get; set; }
-        public string? Address { get; set; }
-        public string? Price { get; set; }
-        public string? Date { get; set; }
-        public string? PropertyType { get; set; }
-        public string? NewBuild { get; set; }
-        public string? Duration { get; set; }
-        public string? Locality { get; set; }
-        public string? Town { get; set; }
-        public string? District { get; set; }
-        public string? County { get; set; }
+        public string Outcode { get; set; } = String.Empty;
+        public string Postcode { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public string Price { get; set; } = String.Empty;
+        public string Date { get; set; } = String.Empty;
+        public string PropertyType { get; set; } = String.Empty;
+        public string NewBuild { get; set; } = String.Empty;
+        public string Duration { get; set; } = String.Empty;
+        public string Locality { get; set; } = String.Empty;
+        public string Town { get; set; } = String.Empty;
+        public string District { get; set; } = String.Empty;
+        public string County { get; set; } = String.Empty;
 
         public override string ToString()
         {
             return $"{Outcode}|{Postcode}|{Address}|{Price}|{Date}|{Locality}|{Town}|{District}|{County}|{PropertyType}|{Duration}|{NewBuild}";
         }
+    }
+
+    public class PropertyPrice
+    {
+        public string Price { get; set; } = String.Empty;
+        public string PriceDate { get; set; } = String.Empty;
+    }
+
+    public class PropertyDetails
+    {
+        public string Address { get; set; } = String.Empty;
+        public string Town { get; set; } = String.Empty;
+        public string Locality { get; set; } = String.Empty;
+        public string Flags { get; set; } = String.Empty;
+        public List<PropertyPrice> Prices { get; set; } = new();
     }
 
     public class UkLandCsvRecord
